41a357dbc8e1309f55027624590c805887e24c54
khushal vadhavana <khushalvadhavana856@gmail.com>
2024-04-12 20:45:15 +0530

    commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+      user="root",
+      password='king007'
+      )
+
+cursorObject = dataBase.cursor()
+
+
+cursorObject.execute("CREATE DATABASE CRM")
+
+print("all done !")
